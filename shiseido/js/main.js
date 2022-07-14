9e2fad47eeafa3b32f28d7d86a410440f4b19ed8
wisberry <wisberry2022@gmail.com>
2022-07-14 12:40:54 +0900

    07/14 first upload

---
+++ shiseido/js/main.js
@@ -0,0 +1,47 @@
+$(function () {
+  var s_cnt = 0;
+
+  $('main .main_slider').on('Init beforeChange', function (e, s, c) {
+    color_set = ['#bae2ee', '#f6d1a4', '#c0022e'];
+    num_set = [0, 2, 1];
+    var cnt = s_cnt;
+    console.log('before_slide: ', c);
+    if (cnt <= 0) {
+      cnt *= -1;
+      cnt %= 3;
+      console.log(cnt);
+      $('.new_product .next_box, .new_product .prev_box').css('background', color_set[num_set[cnt]]);
+    } else if (cnt > 0) {
+      cnt %= 3;
+      $('.new_product .next_box, .new_product .prev_box').css('background', color_set[cnt]);
+    }
+    // $('.new_product .next_box, .new_product .prev_box').css('background', color_set[(c + 1) < 3 ? c + 1 : c = 0]);
+  });
+
+  $('#header_bg_video').YTPlayer({
+    videoURL: 'https://youtu.be/LjRWdVdO50Y',
+    containment: '#header',
+    autoPlay: true,
+    showControls: false,
+    playOnlyIfVisible: true,
+    quality: 'highres',
+    ratio: '4/3',
+  });
+
+  $('main .new_product .main_slider').slick({
+    arrows: false,
+    fade: true,
+    speed: 500,
+  });
+
+  $('.new_product .prev_box').on('click', function () {
+    s_cnt -= 1;
+    $('.main_slider').slick('slickPrev');
+  });
+
+  $('.new_product .next_box').on('click', function () {
+    s_cnt += 1;
+    $('.main_slider').slick('slickNext');
+  });
+})
+
